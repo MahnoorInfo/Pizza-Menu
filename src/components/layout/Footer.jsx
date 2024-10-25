319b17fbed45a40fe15e6caa1cd1a8ef18ba5e21
Mahnoor F <168362825+MahnoorInfo@users.noreply.github.com>
2024-10-26 00:57:46 +0500

    Update Footer.jsx
    current hour change

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,7 @@
 import OrderTime from "../OrderTime";
 
 function Footer() {
-  const currentHour = new Date().getHours();
+  const currentHour = new Date().getHours(); // current hour in 0-23 range
   const openingHour = 12; // opening hour (12PM)
   const closingHour = 22; // closing hour (10PM)
   const isOpen = currentHour >= openingHour && currentHour <= closingHour;
